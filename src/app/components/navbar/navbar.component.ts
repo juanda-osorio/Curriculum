83a849a22db62a718141ceb774560231a18e46da
Juan David <juan_david_osorio2@outlook.es>
2020-11-20 12:35:24 +0100

    menú auto-plegable en cada seccion

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  mostrar: boolean = true;
+
   constructor(/*private _router: Router*/) { }
 
   ngOnInit(): void {
