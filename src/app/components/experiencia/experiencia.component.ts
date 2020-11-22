718fd96c37ea5250e9d294a2445e5de5c9bacc44
Juan David <juan_david_osorio2@outlook.es>
2020-11-22 17:43:08 +0100

    por fin arreglado bien el botón de Fecha asc-desc de experiencia

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { DatosService, Experiencia } from 'src/app/servicios/datos.service';
 import { Router } from '@angular/router';
 
@@ -7,15 +8,23 @@ import { Router } from '@angular/router';
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
 })
-export class ExperienciaComponent implements OnInit {
+export class ExperienciaComponent implements OnInit, OnChanges{
   experiencias: Experiencia[];
   ordenar: boolean;
 
   constructor(
     private _datosService: DatosService,
-    private _router: Router) {
+    private _router: Router,
+    private _breakPointObserver: BreakpointObserver) {
       
       this.ordenar = false;
+  }
+
+  ngOnChanges(){
+    if( this._breakPointObserver.isMatched('(max-width:500px)') ){
+      const botonOrdenarExp = document.querySelector('.boton_exp');
+      botonOrdenarExp.className += " btn-block";      
+    }
   }
   
   ngOnInit(): void {
